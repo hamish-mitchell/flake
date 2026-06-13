f04ba0d569013eaaac033c9f33d93e97a66c70ec
hamish-mitchell <mitchellhamish08@outlook.com>
2026-06-13 21:13:19 +1000

    niri: fix binds

--- home/niri.nix
+++ home/niri.nix
@@ -6,13 +6,15 @@
   programs.niri.settings = {
     animations.enable = true;
 
-    "Mod+Q".action.quit = [];
-    "Mod+Return".action.spawn = lib.getExe config.programs.alacritty.package;
-    "Mod+P".action.spawn = lib.getExe config.programs.fuzzel.package;
+    binds = with config.lib.niri.actions; {
+      "Mod+Q".action.quit = [];
+      "Mod+Return".action.spawn = lib.getExe config.programs.alacritty.package;
+      "Mod+P".action.spawn = lib.getExe config.programs.fuzzel.package;
 
-    "Mod+N".action.focus-column-or-monitor-left = [];
-    "Mod+E".action.focus-window-or-workspace-down = [];
-    "Mod+I".action.focus-window-or-workspace-up = [];
-    "Mod+O".action.focus-column-or-monitor-right = [];
+      "Mod+N".action.focus-column-or-monitor-left = [];
+      "Mod+E".action.focus-window-or-workspace-down = [];
+      "Mod+I".action.focus-window-or-workspace-up = [];
+      "Mod+O".action.focus-column-or-monitor-right = [];
+    };
   };
 }
