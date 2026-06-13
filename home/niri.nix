b9ac6a59ffc218cf9e73abb6531f556102e8b355
willow <42willow@pm.me>
2026-06-13 21:29:21 +1000

    niri: fix binds

--- home/niri.nix
+++ home/niri.nix
@@ -7,18 +7,21 @@
     animations.enable = true;
 
     binds = with config.lib.niri.actions; {
-      "Mod+Q" = { repeat = false; action = close-window; };
-      "Mod+Return" = { action.spawn = "alacritty"; };
-      "Mod+P" = { action.spawn = "fuzzel"; };
+      "Mod+Q" = {
+        repeat = false;
+        action = close-window;
+      };
+      "Mod+Return".action = spawn (lib.getExe config.programs.alacritty.package);
+      "Mod+P".action = spawn (lib.getExe config.programs.fuzzel.package);
 
-      "Mod+Left".action  = focus-column-left;
-      "Mod+Down".action  = focus-window-down;
-      "Mod+Up".action    = focus-window-up;
+      "Mod+Left".action = focus-column-left;
+      "Mod+Down".action = focus-window-down;
+      "Mod+Up".action = focus-window-up;
       "Mod+Right".action = focus-column-right;
-      "Mod+H".action     = focus-column-left;
-      "Mod+J".action     = focus-window-down;
-      "Mod+K".action     = focus-window-up;
-      "Mod+L".action     = focus-column-right;
+      "Mod+H".action = focus-column-left;
+      "Mod+J".action = focus-window-down;
+      "Mod+K".action = focus-window-up;
+      "Mod+L".action = focus-column-right;
     };
   };
 }
