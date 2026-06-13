578cf220c3f2991e011d9a9574018b953b1a4ff2
willow <42willow@pm.me>
2026-06-13 21:09:54 +1000

    niri: basic binds

--- home/niri.nix
+++ home/niri.nix
@@ -1,5 +1,18 @@
 {
+  config,
+  lib,
+  ...
+}: {
   programs.niri.settings = {
     animations.enable = true;
+
+    "Mod+Q".action.quit = [];
+    "Mod+Return".action.spawn = lib.getExe config.programs.alacritty.package;
+    "Mod+P".action.spawn = lib.getExe config.programs.fuzzel.package;
+
+    "Mod+N".action.focus-column-or-monitor-left = [];
+    "Mod+E".action.focus-window-or-workspace-down = [];
+    "Mod+I".action.focus-window-or-workspace-up = [];
+    "Mod+O".action.focus-column-or-monitor-right = [];
   };
 }
